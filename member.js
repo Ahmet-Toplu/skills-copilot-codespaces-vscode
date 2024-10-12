102f6ab4e81b0bfd007947691934c2512bebad77
Ahmet Toplu <72352084+Ahmet-Toplu@users.noreply.github.com>
2024-10-12 18:48:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = new Member();
+  member.skill = 'JavaScript';
+  member.skill = 'React';
+  member.skill = 'Vue';
+  member.skill = 'Angular';
+  member.skill = 'Node.js';
+  member.skill = 'Express';
+  member.skill = 'MongoDB';
+  member.skill = 'SQL';
+  return member;
+}
